5defc3f1921f06d7ac874765500356cce96dfe63
Tavi Nathanson <tavi.nathanson@gmail.com>
2015-07-18 21:23:19 -0400

    Adding RefSeq test

--- test/test_ucsc_gtf.py
+++ test/test_ucsc_gtf.py
@@ -4,10 +4,15 @@ from pyensembl import Genome, GenomeSource
 from nose.tools import eq_
 from os.path import join, dirname
 
-UCSC_PATH = join(dirname(__file__), "data/ucsc.small.gtf")
+UCSC_GENCODE_PATH = join(dirname(__file__), "data/gencode.ucsc.small.gtf")
+UCSC_REFSEQ_PATH = join(dirname(__file__), "data/refseq.ucsc.small.gtf")
 
-def test_ucsc_parsing():
-    genome = Genome(GenomeSource(gtf_path=UCSC_PATH))
+def test_ucsc_gencode():
+    """
+    Testing with a small GENCODE GTF file downloaded from 
+    http://genome.ucsc.edu/cgi-bin/hgTables
+    """
+    genome = Genome(GenomeSource(gtf_path=UCSC_GENCODE_PATH))
     genome.index()
     eq_(len(genome.genes()), 7)
     eq_(len(genome.transcripts()), 7)
@@ -22,3 +27,18 @@ def test_ucsc_parsing():
 
     transcript_1_30564 = genome.transcripts_at_locus(1, 30564)
     eq_(transcript_1_30564[0].id, "uc057aty.1")
+
+def test_ucsc_refseq():
+    """
+    Testing with a small RefSeq GTF file downloaded from
+    http://genome.ucsc.edu/cgi-bin/hgTables
+    """
+    genome = Genome(GenomeSource(gtf_path=UCSC_REFSEQ_PATH))
+    genome.index()
+    eq_(len(genome.genes()), 2)
+    eq_(len(genome.transcripts()), 2)
+
+    genes_at_locus = genome.genes_at_locus(1, 67092176)
+    eq_(len(genes_at_locus), 2)
+    ids = set([gene.id for gene in genes_at_locus])
+    eq_(set(["NM_001276352", "NR_075077"]), ids)
