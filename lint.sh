f52c73c4c7360da6399bc840d25c3dcab5338eec
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2015-12-18 12:10:36 -0500

    disable pylint errors that cause false positives

--- lint.sh
+++ lint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -o errexit
 
+
+# getting false positives due to this issue with pylint:
+# https://bitbucket.org/logilab/pylint/issues/701/false-positives-with-not-an-iterable-and
+
 find . -name '*.py' \
   | xargs pylint \
   --errors-only \
-  --disable=print-statement
+  --disable=print-statement,unsubscriptable-object,not-an-iterable
 
 echo 'Passes pylint check'
