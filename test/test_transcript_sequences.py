277574fdb225c97004134c00d9a2293cd6f9e18c
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2018-01-17 20:19:51 -0500

    changed test using 83 to use 87

--- test/test_transcript_sequences.py
+++ test/test_transcript_sequences.py
@@ -5,35 +5,12 @@ and `reverse_complement`
 
 from __future__ import absolute_import
 from nose.tools import eq_
-from pyensembl import cached_release
+from pyensembl import ensembl_grch38
 
-ensembl54 = cached_release(54)
-ensembl83 = cached_release(83)
-
-def test_transcript_sequence_ensembl54():
-    seq = ensembl54.transcript_sequence("ENST00000321606")
-    assert len(seq) == 414, \
-        "Expected transcript ENST00000321606 to have 414nt, got %s : %d" % (
-            seq, len(seq))
-    nucleotide_lines = [
-        "CATGTCACCCACCTTCAGGCGGCCCAAGACACTGCGACTCCGGAGGCAGCCCAGATATCCTCGGAAGAG",
-        "CACCCCCAGGAGAAACAAGCTTGGCCACTATGCTATCATCAAGTTTCCGCTGACCACTGAGTCGGCCGT",
-        "GAAGAAGATAGAAGAAAACAACACGCTTGTGTTCACTGTGGATGTTAAAGCCAACAAGCACCAGATCAG",
-        "ACAGGCTGTGAAGAAGCTCTATGACAGTGATGTGGCCAAGGTCACCACCCTGATTTGTCCTGATAAAGA",
-        "GAACAAGGCATATGTTCGACTTGCTCCTGATTATGATGCTTTCGATGTTGTAACAAAATTGGGATCACC",
-        "TAAACTGAGTCCAGCTGGCTAACTCTAAATATATGTGTATCTTTTCAGCATAAAAAAATAATGTTTTTC"
-    ]
-    full_transcript_sequence = "".join(nucleotide_lines)
-    eq_(str(seq), full_transcript_sequence)
-
-    # now get the same sequence via a Transcript object
-    eq_(ensembl54.transcript_by_id("ENST00000321606").sequence, seq)
-
-
-def test_transcript_sequence_ensembl83():
+def test_transcript_sequence_ensembl_grch38():
     # extremely short TRD gene
-    seq = ensembl83.transcript_sequence("ENST00000448914")
+    seq = ensembl_grch38.transcript_sequence("ENST00000448914")
     expected = "ACTGGGGGATACG"
     eq_(seq, expected)
     # now try via a Transcript object
-    eq_(ensembl83.transcript_by_id("ENST00000448914").sequence, expected)
+    eq_(ensembl_grch38.transcript_by_id("ENST00000448914").sequence, expected)
