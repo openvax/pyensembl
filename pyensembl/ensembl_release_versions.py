489498c75863d1caf26516e0551f5780a1ddf29d
Jaan Kasak <jaan.kasak@abzu.ai>
2022-01-06 10:26:03 +0100

    bump ensembl release version

--- pyensembl/ensembl_release_versions.py
+++ pyensembl/ensembl_release_versions.py
@@ -13,7 +13,7 @@
 from __future__ import print_function, division, absolute_import
 
 MIN_ENSEMBL_RELEASE = 54
-MAX_ENSEMBL_RELEASE = 104
+MAX_ENSEMBL_RELEASE = 105
 
 def check_release_number(release):
     """
