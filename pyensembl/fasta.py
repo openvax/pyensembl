14634449db472812041ad2a50480fbfea1ef3426
Chang Ye <yech1990@gmail.com>
2022-08-18 16:31:06 -0500

    fix naming

--- pyensembl/fasta.py
+++ pyensembl/fasta.py
@@ -51,9 +51,10 @@ def _parse_header_id(line):
     # .e.g.
     # "ENST00000448914.1" instead of "ENST00000448914"
     # So now we have to parse out the identifier
-    dot_index = identifier.find(b".")
-    if dot_index >= 0:
-        identifier = identifier[:dot_index]
+    if identifier.startswith(b"ENS"):
+        dot_index = identifier.find(b".")
+        if dot_index >= 0:
+            identifier = identifier[:dot_index]
 
     return identifier.decode("ascii")
 
