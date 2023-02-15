aa11b29eb0b6820217eeac8654e5e694f20fafb9
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2023-02-15 15:36:55 -0500

    added fruit fly

--- pyensembl/species.py
+++ pyensembl/species.py
@@ -264,3 +264,14 @@ pig = Species.register(
     latin_name="sus_scrofa",
     synonyms=["pig"],
     reference_assemblies={"Sscrofa11.1": (75, MAX_ENSEMBL_RELEASE)})
+
+fly = Species.register(
+    latin_name="drosophila_melanogaster",
+    synonyms=["drosophila", "fruit fly", "fly"],
+    reference_assemblies={
+         "BDGP5": (75, 78),
+         "BDGP6": (79, 95),
+         "BDGP6.22": (96, 98),
+         "BDGP6.28": (99, 102),
+         "BDGP6.32": (103, MAX_ENSEMBL_RELEASE)
+    })
